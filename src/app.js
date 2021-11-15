d450d0322191f1be6bc3a3d2266cb5ebcf3b92a8
Diego <dchiapa@est.frba.utn.edu.ar>
2021-11-15 12:07:49 -0300

    update app

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import { render } from "react-dom";
 import "./app.scss";
 import { Form } from "./components/Form/Form";
+import { List } from "./components/List/List";
 
 const App = () => {
   const [text, setText] = useState("");
+  const [items, setItems] = useState([]);
   return (
     <>
       <h1>Todo App</h1>
@@ -12,7 +14,8 @@ const App = () => {
         Versión simple, permite agregar, borrar y marcar los elementos
         completados en un listado. El listado se pierde al cerrar el navegador.
       </p>
-      <Form text={text} setText={setText} />
+      <Form items={items} setItems={setItems} setText={setText} text={text} />
+      <List items={items} />
     </>
   );
 };
