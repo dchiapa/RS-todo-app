8dad78fd1666e2334c4c28a41e633c55e8f76223
Diego <dchiapa@est.frba.utn.edu.ar>
2021-11-15 22:07:48 -0300

    update List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,37 @@
 import React from "react";
+import "./List.scss";
+import { ListItem } from "./ListItem";
 
-export const List = ({ items }) => {
-  console.log(items);
-  return <ul></ul>;
+export const List = ({ items, setItems }) => {
+  let itemsList = [];
+
+  const removeItem = (id) => {
+    itemsList = items.filter((item) => item.index != id);
+    setItems(itemsList);
+  };
+
+  const completeItem = (id) => {
+    itemsList = items.map((item) => {
+      if (item.index == id) {
+        item.completed = !item.completed;
+      }
+      return item;
+    });
+    setItems(itemsList);
+  };
+
+  return (
+    <ul>
+      {items.map((item) => {
+        return (
+          <ListItem
+            completeItem={completeItem}
+            item={item}
+            key={item.index}
+            removeItem={removeItem}
+          />
+        );
+      })}
+    </ul>
+  );
 };
