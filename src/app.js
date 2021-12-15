2e9136869452920abdf8b3a14aad462b5f39bb68
Diego <dchiapa@est.frba.utn.edu.ar>
2021-12-15 12:18:56 -0300

    init V1.2

--- src/app.js
+++ src/app.js
@@ -1,21 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { render } from "react-dom";
 import "./app.scss";
-import { Form } from "./components/Form/Form";
-import { List } from "./components/List/List";
 
 const App = () => {
-  const [text, setText] = useState("");
-  const [items, setItems] = useState([]);
   return (
     <>
       <h1>Todo App</h1>
       <p>
-        Versión simple, permite agregar, borrar y marcar los elementos
-        completados en un listado. El listado se pierde al cerrar el navegador.
+        Versión que permite agregar, borrar y marcar los elementos completados
+        en un listado. El listado se pierde al cerrar el navegador.
       </p>
-      <Form items={items} setItems={setItems} setText={setText} text={text} />
-      <List items={items} setItems={setItems} />
     </>
   );
 };
