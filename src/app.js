463f9e75dfce7935d0a6d117f05006ab84939093
Diego <dchiapa@est.frba.utn.edu.ar>
2021-12-16 16:43:23 -0300

    update app file

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import "./app.scss";
-import Container from "./components/Container";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 const App = () => {
+  const [list, setList] = useState([]);
+
+  const handleAddItem = (item) => {
+    setList([...list, item]);
+  };
+
   return (
     <>
-      <h1>Todo App</h1>
-      <Container />
+      <h1>Todo List</h1>
+      <TodoForm handleAddItem={handleAddItem} />
+      <TodoList list={list} setList={setList} />
     </>
   );
 };
