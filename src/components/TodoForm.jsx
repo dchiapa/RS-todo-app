99387c39ad9c546ac119f579d8da7f734fe6a360
Diego <dchiapa@est.frba.utn.edu.ar>
2021-12-16 16:46:12 -0300

    add form controls

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const TodoForm = ({ handleAddItem }) => {
-  return <form onSubmit={handleFormSubmit} className="form__container"></form>;
+  return (
+    <form onSubmit={handleFormSubmit} className="form__container">
+      <input
+        type="text"
+        className="form__input"
+        value={todo}
+        onChange={handleTextChange}
+      />
+      <button className="form__button" disabled={todo ? "" : "disable"}>
+        Agregar
+      </button>
+    </form>
+  );
 };
 
 export default TodoForm;
