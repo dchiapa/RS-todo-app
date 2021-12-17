3733144ca27b4e758744a7a43e7c0d76a03e91b1
Diego <dchiapa@est.frba.utn.edu.ar>
2021-12-17 12:13:56 -0300

    update localstorage logic

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { render } from "react-dom";
 import "./app.scss";
 import TodoForm from "./components/TodoForm/TodoForm";
 import TodoList from "./components/TodoList/TodoList";
 
 const App = () => {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(() => {
+    const savedData = localStorage.getItem("todoList");
+    const initialValue = JSON.parse(savedData);
+    return initialValue || [];
+  });
 
   const handleAddItem = (item) => {
     setList([...list, item]);
   };
+
+  useEffect(() => {
+    localStorage.setItem("todoList", JSON.stringify(list));
+  }, [list]);
 
   return (
     <>
