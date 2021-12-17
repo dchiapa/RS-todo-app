aef3c08fc694ddb790d0f04b66d791ecf877c5c0
Diego <dchiapa@est.frba.utn.edu.ar>
2021-12-17 12:12:55 -0300

    add TodoList logic and render

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
 const TodoList = ({ list, setList }) => {
-  return <div className="list__container"></div>;
+  const handleDelete = () => {
+    let deletedList = list.filter((item) => item.done != true);
+    setList(deletedList);
+  };
+
+  const handleUpdate = (id) => {
+    let updateList = list.map((item) => {
+      if (item.id === id) {
+        item.done = !item.done;
+      }
+      return item;
+    });
+    setList(updateList);
+  };
+
+  return (
+    <div>
+      {list.length > 0 ? (
+        <ul className="list__container">
+          {list.length > 0 &&
+            list.map((item) => {
+              return (
+                <TodoItem
+                  key={item.id}
+                  item={item}
+                  handleUpdate={handleUpdate}
+                />
+              );
+            })}
+        </ul>
+      ) : null}
+      {list.length > 0 ? (
+        <button className="list__button" onClick={handleDelete}>
+          Borrar tareas terminadas
+        </button>
+      ) : null}
+    </div>
+  );
 };
 
 export default TodoList;
