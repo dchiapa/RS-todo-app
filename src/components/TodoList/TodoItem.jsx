f6ac5c51fe3f63c4a6e2dae1a14b98017f97b826
Diego <dchiapa@est.frba.utn.edu.ar>
2021-12-17 13:53:11 -0300

    add delete item handler

--- src/components/TodoList/TodoItem.jsx
+++ src/components/TodoList/TodoItem.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 import "./TodoItem.scss";
 
-const TodoItem = ({ handleUpdate, item }) => {
-  const handleClick = () => {
+const TodoItem = ({ handleDelete, handleUpdate, item }) => {
+  const handleUpdateClick = () => {
     handleUpdate(item.id);
+  };
+  const handleDeleteClick = () => {
+    handleDelete(item.id);
   };
 
   return (
-    <li className="item" id={item.id} onClick={handleClick}>
+    <li className="item" id={item.id}>
       <input
         type="checkbox"
         checked={item.done}
         readOnly
         className={item.done ? "item__checkbox-complete" : "item__checkbox"}
+        onClick={handleUpdateClick}
       />
-      <span className={item.done ? "item__text-complete" : "item__text"}>
+      <span
+        className={item.done ? "item__text-complete" : "item__text"}
+        onClick={handleUpdateClick}
+      >
         {item.todo}
       </span>
+      <button className="item__btn-delete" onClick={handleDeleteClick}>
+        x
+      </button>
     </li>
   );
 };
